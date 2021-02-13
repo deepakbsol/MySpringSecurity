12f0d945e6791855c75f3eae54c85abcab3b98cf
deepak <kumar.dpk03@gmail.com>
2021-02-13 12:33:07 +0530

    removed all

--- src/main/java/com/spring/security/config/AuthUserDao.java
+++
@@ -1,9 +0,0 @@
-package com.spring.security.config;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthUserDao {
-
-	public UserEntity getUserEntity(String username);
-}
